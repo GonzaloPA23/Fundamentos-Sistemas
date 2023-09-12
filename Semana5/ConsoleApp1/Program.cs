f96ab0de4c8273af383f0c0aae7b015336271923
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-09-12 00:34:43 -0500

    Iniciando la semana 05

---
+++ Semana5/ConsoleApp1/Program.cs
@@ -0,0 +1,130 @@
+﻿//using System;
+//using System.Collections.Generic;
+
+//class Student
+//{
+//    public string Name { get; set; }
+//    public int Age { get; set; }
+//    public List<string> Courses { get; set; }
+
+//    public Student(string name, int age)
+//    {
+//        Name = name;
+//        Age = age;
+//        Courses = new List<string>();
+//    }
+
+//    public void AddCourse(string courseName)
+//    {
+//        //Courses.Add(courseName);
+//    }
+
+//    public override string ToString()
+//    {
+//        return $"Nombre: {Name}, Edad: {Age}, Curso: {string.Join(", ", Courses)}";
+//    }
+//}
+
+//class Program
+//{
+//    static List<Student> students = new List<Student>();
+
+//    static void Main(string[] args)
+//    {
+//        while (true)
+//        {
+//            Console.WriteLine("Multilista de Estudiantes y Cursos");
+//            Console.WriteLine("1. Agregar Estudiante");
+//            Console.WriteLine("2. Agregar Curso a Estudiante");
+//            Console.WriteLine("3. Mostrar Estudiantes");
+//            Console.WriteLine("4. Salir");
+//            Console.Write("Digite Opcion: ");
+
+//            int choice;
+//            if (!int.TryParse(Console.ReadLine(), out choice))
+//            {
+//                Console.WriteLine("Entrada no válida. Introduce un número del menú.");
+//                continue;
+//            }
+
+//            switch (choice)
+//            {
+//                case 1:
+//                    AddStudent();
+//                    break;
+//                case 2:
+//                    AddCourseToStudent();
+//                    break;
+//                case 3:
+//                    ShowStudents();
+//                    break;
+//                case 4:
+//                    Environment.Exit(0);
+//                    break;
+//                default:
+//                    Console.WriteLine("Opción no válida. Introduce un número válido del menú.");
+//                    break;
+//            }
+//        }
+//    }
+
+//    static void AddStudent()
+//    {
+//        Console.Write("Nombre del estudiante: ");
+//        string name = Console.ReadLine();
+//        Console.Write("Edad del estudiante: ");
+//        if (!int.TryParse(Console.ReadLine(), out int age))
+//        {
+//            Console.WriteLine("Edad no válida. Introduce un número.");
+//            return;
+//        }
+
+//        Student student = new Student(name, age);
+//        students.Add(student);
+//        Console.WriteLine("Estudiante agregado exitosamente.");
+//    }
+
+//    static void AddCourseToStudent()
+//    {
+//        if (students.Count == 0)
+//        {
+//            Console.WriteLine("No hay estudiantes registrados.");
+//            return;
+//        }
+
+//        Console.WriteLine("Lista de Estudiantes:");
+//        for (int i = 0; i < students.Count; i++)
+//        {
+//            Console.WriteLine($"{i + 1}. {students[i].Name}");
+//        }
+
+//        Console.Write("Selecciona el número de estudiante al que deseas agregar un curso: ");
+//        if (!int.TryParse(Console.ReadLine(), out int studentIndex) || studentIndex < 1 || studentIndex > students.Count)
+//        {
+//            Console.WriteLine("Selección no válida. Introduce un número válido.");
+//            return;
+//        }
+
+//        Student selectedStudent = students[studentIndex - 1];
+//        Console.Write("Nombre del curso a agregar: ");
+//        string courseName = Console.ReadLine();
+
+//        selectedStudent.AddCourse(courseName);
+//        Console.WriteLine($"Curso '{courseName}' agregado al estudiante '{selectedStudent.Name}'.");
+//    }
+
+//    static void ShowStudents()
+//    {
+//        if (students.Count == 0)
+//        {
+//            Console.WriteLine("No hay estudiantes registrados.");
+//            return;
+//        }
+
+//        Console.WriteLine("Lista de Estudiantes:");
+//        for (int i = 0; i < students.Count; i++)
+//        {
+//            Console.WriteLine($"{i + 1}. {students[i]}");
+//        }
+//    }
+//}
---
+++ Semana5/ConsoleApp1/Program2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Definir la clase Compra para almacenar información sobre cada compra
+class Compra
+{
+    public string DniCliente { get; set; }
+    public decimal Monto { get; set; }
+    public DateTime Fecha { get; set; }
+}
+
+// Definir la clase Caja para almacenar información sobre cada caja
+class Caja
+{
+    public int Codigo { get; set; }
+    public string NombreCajero { get; set; }
+    public List<Compra> Compras { get; } = new List<Compra>();
+}
+
+class Program
+{
+    static List<Caja> cajas = new List<Caja>();
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Seleccione una opción:");
+            Console.WriteLine("1. Crear una Caja");
+            Console.WriteLine("2. Registrar una compra");
+            Console.WriteLine("3. Listar las compras de una caja");
+            Console.WriteLine("4. Obtener la caja con más compras");
+            Console.WriteLine("5. Listar las cajas de un cliente");
+            Console.WriteLine("6. Obtener el o los clientes con menor monto de compra");
+            Console.WriteLine("7. Obtener el monto total de compras de todas las cajas");
+            Console.WriteLine("8. Salir");
+            Console.Write("Eliga una Opcion: ");
+
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:CrearCaja();break;
+                case 2:RegistrarCompra(); break;
+                //case 3:ListarComprasDeCaja();break;
+                //case 4: ObtenerCajaConMasCompras(); break;
+                //case 5:ListarCajasDeCliente();break;
+                //case 6:ObtenerClientesConMenorMonto(); break;
+               // case 7: ObtenerMontoTotalDeCompras();break;
+                case 8: Environment.Exit(0); break;
+                default: Console.WriteLine("Opción no válida. Intente de nuevo.");break;
+            }
+        }
+    }
+
+    static void CrearCaja()
+    {
+        Console.Write("Ingrese el código de la caja: ");
+        int codigo = int.Parse(Console.ReadLine());
+
+        if (cajas.Any(c => c.Codigo == codigo))
+        {
+            Console.WriteLine("Ya existe una caja con ese código.");
+        }
+        else
+        {
+            Console.Write("Ingrese el nombre del cajero: ");
+            string nombreCajero = Console.ReadLine();
+
+            Caja caja = new Caja { Codigo = codigo, NombreCajero = nombreCajero };
+            cajas.Add(caja);
+            Console.WriteLine("Caja creada exitosamente.");
+        }
+    }
+
+    static void RegistrarCompra()
+    {
+        Console.Write("Ingrese el código de la caja: ");
+        int codigoCaja = int.Parse(Console.ReadLine());
+
+        Caja caja = cajas.FirstOrDefault(c => c.Codigo == codigoCaja);
+        if (caja == null)
+        {
+            Console.WriteLine("No se encontró una caja con ese código.");
+        }
+        else
+        {
+            Console.Write("Ingrese el DNI del cliente: ");
+            string dniCliente = Console.ReadLine();
+
+            Console.Write("Ingrese el monto de la compra: ");
+            decimal montoCompra = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la fecha de la compra (yyyy-MM-dd): ");
+            DateTime fechaCompra = DateTime.Parse(Console.ReadLine());
+
+            Compra compra = new Compra { DniCliente = dniCliente, Monto = montoCompra, Fecha = fechaCompra };
+            caja.Compras.Add(compra);
+            Console.WriteLine("Compra registrada exitosamente.");
+        }
+    }
+
+    // Implementa el resto de las funciones según los requerimientos funcionales
+}
