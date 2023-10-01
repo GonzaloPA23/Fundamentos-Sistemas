ff506bd082612d104d1b7de25446287a30bf4409
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-10-01 05:34:01 -0500

    Realicé el formulario de menu principal

--- TP/TP/TP/Program.cs
+++ TP/TP/TP/Program.cs
@@ -1,3 +1,5 @@
+using CapaPresentacion;
+
 namespace TP
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace TP
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frmInicio());
+            Application.Run(new frmMenuPrincipal());
         }
     }
 }
---
+++ TP/TP/TP/frmMenuPrincipal.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace CapaPresentacion
+{
+    partial class frmMenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button1.AutoEllipsis = true;
+            button1.BackColor = Color.FromArgb(182, 113, 137);
+            button1.Cursor = Cursors.Hand;
+            button1.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 0, 0);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(169, 100, 122);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(164, 709);
+            button1.Name = "button1";
+            button1.Size = new Size(345, 58);
+            button1.TabIndex = 8;
+            button1.Text = "Registrarse";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button2.AutoEllipsis = true;
+            button2.BackColor = Color.FromArgb(222, 108, 148);
+            button2.Cursor = Cursors.Hand;
+            button2.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 0, 0);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.FromArgb(206, 113, 142);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(164, 630);
+            button2.Name = "button2";
+            button2.Size = new Size(345, 60);
+            button2.TabIndex = 9;
+            button2.Text = "Iniciar sesión";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(225, 261);
+            label3.Name = "label3";
+            label3.Size = new Size(246, 28);
+            label3.TabIndex = 5;
+            label3.Text = "Chiclayo - Lima y Viceversa";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(270, 237);
+            label2.Name = "label2";
+            label2.Size = new Size(160, 25);
+            label2.TabIndex = 6;
+            label2.Text = "SALIDAS DIARIAS:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(118, 118);
+            label1.Name = "label1";
+            label1.Size = new Size(437, 108);
+            label1.TabIndex = 7;
+            label1.Text = "Bienvenido a La Perla \r\ndel Altomayo";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            pictureBox2.Image = Properties.Resources.bus_menu;
+            pictureBox2.Location = new Point(144, 328);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(381, 258);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            pictureBox1.Image = Properties.Resources.logo_perla_del_altomayo;
+            pictureBox1.Location = new Point(246, -28);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(201, 191);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // frmMenuPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(235, 235, 233);
+            ClientSize = new Size(691, 836);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            Name = "frmMenuPrincipal";
+            Opacity = 0.95D;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "frmMenuPrincipal";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+    }
+}
