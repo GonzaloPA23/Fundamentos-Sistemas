367223d2b6bc4a048f6ab44d00c0afa068fa2fe5
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2023-11-01 01:44:01 -0500

    Realicé el lab15 con conexion a base de datos

---
+++ Semana11/Lab15/Datos/Database.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    internal class Database
+    {
+        private SqlConnection conn = new SqlConnection("Data Source = localhost; Initial Catalog = db_lab15; Integrated Security = True");
+        public SqlConnection ConectaDb()
+        {
+            try
+            {
+                conn.Open();
+                return conn;
+            }
+            catch (SqlException e)
+            {
+                return null;
+            }
+        }
+        public void DesconectaDb()
+        {
+            conn.Close();
+        }
+    }
+}
